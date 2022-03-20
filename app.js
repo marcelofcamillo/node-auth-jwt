87d0fe22d9a8c2c00b3e65b01883d292fb0c2bc0
Marcelo Camillo <marcelocamillo18@gmail.com>
2022-03-20 14:24:51 -0300

    Finalização

--- app.js
+++ app.js
@@ -72,6 +72,15 @@ function checkToken(req, res, next) {
   const token = authHeader && authHeader.split(' ')[1];
 
   if (!token) res.status(401).json({ msg: 'Acesso negado!' });
+
+  try {
+    const secret = process.env.SECRET;
+    jwt.verify(token, secret);
+
+    next();
+  } catch (error) {
+    res.status(400).json({ msg: 'Token inválido!' });
+  }
 }
 
 // login user
