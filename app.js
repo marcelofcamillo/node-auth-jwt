885ad0112c5b3f4148a608269619c7a3b310ce4e
Marcelo Camillo <marcelocamillo18@gmail.com>
2022-03-20 13:50:20 -0300

    Rota de registro de usuário

--- app.js
+++ app.js
@@ -4,14 +4,29 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
+// models
+import User from './models/User.js';
+
 dotenv.config();
 
 const app = express();
 
+// config JSON response
+app.use(express.json());
+
+// open route - public route
 app.get('/', (req, res) => {
   res.status(200).json({ msg: 'Bem vindo a nossa API!' });
 });
 
+// register user
+app.post('/auth/register', async (req, res) => {
+  const { name, email, password, confirmPassword } = req.body;
+
+  res.status(200).json({ msg: 'Autenticado!' });
+});
+
+// credentials
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASS;
 
